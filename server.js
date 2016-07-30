aba503adab8d4dcd7ee57377335723dfd4dcc58a
leverage2014 <myletter428@gmail.com>
2016-07-30 12:15:37 -0400

    test middleware

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ var app = express();
 
 const PORT = process.env.PORT || 9000;
 
-app.use(function(req, res, next) {
-	console.log('--- middleware ----');
-	console.log(req.hostname);
-	console.log(req.url);
-	console.log('http header');
-	console.log(req.headers['x-forwarded-proto']);
+// app.use(function(req, res, next) {
+// 	console.log('--- middleware ----');
+// 	console.log(req.hostname);
+// 	console.log(req.url);
+// 	console.log('http header');
+// 	console.log(req.headers['x-forwarded-proto']);
 
-	if (req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else {
-		console.log('http://' + req.hostname + req.url);
-		res.redirect('http://' + req.hostname + req.url);
-	}
-});
+// 	if (req.headers['x-forwarded-proto'] === 'http') {
+// 		next();
+// 	} else {
+// 		console.log('http://' + req.hostname + req.url);
+// 		res.redirect('http://' + req.hostname + req.url);
+// 	}
+// });
 
 app.use(express.static("public"));
 
